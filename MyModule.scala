4cdeb98c68b47f08c02d0b6505b260275e522f55
Scott Love <scott.love@gmail.com>
2015-10-04 17:44:30 -0400

    2.1 Fibonacci Recursive Loop

--- MyModule.scala
+++ MyModule.scala
@@ -1,27 +1,36 @@
 object MyModule {
-    def abs(n: Int): Int = {
-        if(n < 0)
-            -n
-        else
-            n
+  def abs(n: Int): Int = {
+    if (n < 0)
+      -n
+    else
+      n
+  }
+
+  private def formatAbs(x: Int) = {
+    val msg = "The absolute value of %d is %d"
+    msg.format(x, abs(x))
+  }
+
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else go(n - 1, n * acc)
+
+    go(n, 1)
+  }
+
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, num: Int, fibnum: Int): Int = {
+      if (n <= 0) fibnum
+      else go(n - 1, fibnum, fibnum + num)
     }
+    go(n, 1, 0)
+  }
 
-    private def formatAbs(x: Int) = {
-        val msg = "The absolute value of %d is %d"
-        msg.format(x, abs(x))
-    }
-
-    def factorial(n: Int): Int = {
-        @annotation.tailrec
-        def go(n: Int, acc: Int) : Int =
-          if(n <=0 ) acc
-          else go(n-1, n*acc)
-
-        go(n, 1)
-    }
-
-    def main(args: Array[String]): Unit =
-        println(formatAbs(-42))
+  def main(args: Array[String]): Unit =
+    println(formatAbs(-42))
 
 
 }
