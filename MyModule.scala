552d3f5b0a72abb0fecb3bdddbf813898774a532
Scott Love <scottlove@unboxedtechnology.com>
2015-10-05 11:10:04 -0400

    More Generics, Curry, and Compose

--- MyModule.scala
+++ MyModule.scala
@@ -24,10 +24,33 @@ object MyModule {
     go(n, 1, 0)
   }
 
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+    def loop(i: Int): Boolean = {
+      if(i >= as.length - 1) true
+      else if(gt(as(i), as(i+1))) false
+      else loop(i + 1)
+    }
+    loop(0)
+  }
+
+  def curry[A,B,C](f: (A,B) => C): A => (B => C) = {
+    a => b => f(a,b)
+
+  }
+
+  def uncurry[A,B,C](f: A => B => C): (A,B) => C ={
+    (a, b) => f(a)(b)
+  }
+
+  def compose[A,B,C](f: B => C, g: A => B): A => C = {
+    (a) => f(g(a))
+  }
+
   private def formatResult(name: String, n: Int, f: Int => Int) = {
     val msg = "The %s of %d is %d"
     msg.format(name, n, f(n))
   }
+
 
   def main(args: Array[String]): Unit = {
     println(formatResult("absolute value",-42, (x: Int) => abs(x)))
