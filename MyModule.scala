b23251a053b29a9a7a9fcbb66cc2ac3a0156007e
Scott Love <scott.love@gmail.com>
2015-10-05 08:13:44 -0400

    Updated with Generic FormatResult Function

--- MyModule.scala
+++ MyModule.scala
@@ -6,11 +6,6 @@ object MyModule {
       n
   }
 
-  private def formatAbs(x: Int) = {
-    val msg = "The absolute value of %d is %d"
-    msg.format(x, abs(x))
-  }
-
   def factorial(n: Int): Int = {
     @annotation.tailrec
     def go(n: Int, acc: Int): Int =
@@ -29,8 +24,16 @@ object MyModule {
     go(n, 1, 0)
   }
 
-  def main(args: Array[String]): Unit =
-    println(formatAbs(-42))
+  private def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d"
+    msg.format(name, n, f(n))
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(formatResult("absolute value",-42, (x: Int) => abs(x)))
+    println(formatResult("factorial", 7, (x: Int) => factorial(x)))
+  }
+
 
 
 }
